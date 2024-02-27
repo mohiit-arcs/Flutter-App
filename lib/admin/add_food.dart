57c4ae2f7f935f413c89bb448d7d15712a54a222
Mohit Gupta <mohit.gupta@arcsinfotech.com>
2024-02-27 19:00:14 +0530

    admin home screen created

---
+++ lib/admin/add_food.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddFood extends StatefulWidget {
+  const AddFood({super.key});
+
+  @override
+  State<AddFood> createState() => _AddFoodSate();
+}
+
+class _AddFoodSate extends State<AddFood> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/admin/admin_home.dart
+++ lib/admin/admin_home.dart
@@ -1,15 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:fooddeliveryapp/admin/add_food.dart';
 import 'package:fooddeliveryapp/widget/widget_support.dart';
 
 class AdminHome extends StatefulWidget {
   const AdminHome({super.key});
 
+  @override
   State<AdminHome> createState() => _AdminHomeState();
 }
 
 class _AdminHomeState extends State<AdminHome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                "Home Admin",
+                style: AppWidget.headlineTextFieldStyle(),
+              ),
+            ),
+            const SizedBox(
+              height: 50.0,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AddFood()));
+              },
+              child: Material(
+                elevation: 10.0,
+                borderRadius: BorderRadius.circular(10),
+                child: Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: Image.asset(
+                            "images/food.jpg",
+                            height: 100,
+                            width: 100,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 30.0,
+                        ),
+                        const Text(
+                          "Add Food Items",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
